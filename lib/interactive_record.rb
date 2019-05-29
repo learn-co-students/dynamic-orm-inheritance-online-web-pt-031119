b7b959d10f3c1af43d4998871fb470d5b04b8a61
Mark Kanko <mkanko66@gmail.com>
2019-05-29 08:08:42 -0700

    completed dynamic-orm-inheritance-lab

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
 
@@ -28,6 +29,7 @@ class InteractiveRecord
 
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    #binding.pry 
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
@@ -49,8 +51,9 @@ class InteractiveRecord
   end
 
 def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
-  DB[:conn].execute(sql, name)
+  sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+  #binding.pry 
+  DB[:conn].execute(sql) 
 end
 
 end
